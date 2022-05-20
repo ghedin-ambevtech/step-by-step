aa25887049754204482fe59f50dd0ed2018b71d1
Alison J. Ghedin <alison.ghedin@ambevtech.com.br>
2022-05-19 21:47:30 -0300

    adicionando arquivo para testes aleatórios

--- task/teste.py
+++ task/teste.py
@@ -1,10 +1,48 @@
+#!/bin/python3
 
-def cumsum(*args):
-    t = []
-    soma = 0
-    for item in args:
-        soma += item
-        t.append(soma)
-    return t
+import math
+import os
+import random
+import re
+import sys
 
-print(cumsum(1, 5, 6, 15, 23, 2))
+#
+# Complete the 'compareTriplets' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER_ARRAY b
+#
+
+
+
+
+
+def compareTriplets(a, b):
+    points = []
+    alice = 0
+    bob = 0
+    for n in range(2):
+        if a[n] > b[n]:
+            alice += 1
+        elif a[n] < b[n]:
+            bob += 1
+    points[0] = alice
+    points[1] = bob
+    return points
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    a = list(map(int, input().rstrip().split()))
+
+    b = list(map(int, input().rstrip().split()))
+    print(a)
+    print(b)
+    result = compareTriplets(a, b)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
