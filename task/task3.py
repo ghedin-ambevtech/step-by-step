35ac3f81617b799157e1e12f93ed5edb6358ae6c
Alison J. Ghedin <alison.ghedin@ambevtech.com.br>
2022-05-20 18:05:36 -0300

    exercicio refatorado

--- task/task3.py
+++ task/task3.py
@@ -1,94 +1,121 @@
 """
-Crie um sistema de cadastro de clientes que seja possível, criar, listar, atualizar e deletar um cliente.
+Crie um sistema de cadastro de customers que seja possível, criar, list, atualizar e deletar um customer.
 
 Sistema em DOS, utilizando o que aprendemos até agora, armazene em memória em um lista!
 
 """
-from time import sleep, time
+from time import sleep
 from uuid import uuid4
+from typing import NoReturn
+
+customers = []
 
 
-def incluir_cliente(nome:str):
+def validate_customers() -> NoReturn:
+    if len(customers) == 0:
+        print("Nenhum customer cadastrado!")
+
+
+def list_customers() -> NoReturn:
+    list_of_customers = [customer for customer in customers]
+    print(f"Lista de clientes: {list_of_customers}")
+
+
+def create_customer() -> NoReturn:
     """
      Função de cadastramento de clientes
     """
-    cliente = {}
-    cliente["Nome"] = nome.title()
-    id_cliente = str(uuid4())[0:5]
-    cliente["Id"] = id_cliente
-    clientes.append(cliente)
-    print(f"Cliente {nome} cadastrado com sucesso!")
+    customer = {}
+    id_customer = str(uuid4())[0:5]
+    customer["Id"] = id_customer
+    name = input("Nome do cliente: ").strip()
+    customer["Name"] = name.title()
+    customers.append(customer)
+    print(f"Cliente {customer['Name']} cadastrado com sucesso!")
 
 
-def alterar_cliente(nome: str):
+def update_customer() -> NoReturn:
     """
      Função de alteração cadastral de clientes
     """
-    alterado = None
-    for cliente in clientes:
-        if id_cliente == cliente.get('Id'):
-            nome = input(f"Você quer alterar o nome de {cliente.get('Nome')} para qual nome ? ").strip()
-            cliente['Nome'] = nome
-            print(f"Cliente {id_cliente} alterado com sucesso!")
-            alterado = True
+    print("Atualizar dados de um cliente")
+    id_customer = input("Digite o id do cliente que deseja alterar: ").strip()
+    updated = None
+    for customer in customers:
+        if id_customer == customer.get('Id'):
+            name = input(f"Você quer alterar o name de {customer.get('Nome')} para qual nome ? ").strip()
+            customer['Name'] = name.title()
+            print(f"Cliente {id_customer} alterado com sucesso!")
+            updated = True
             break
-    if not alterado:
-        print(f"Id {id_cliente} não encontrado!")
+    if not updated:
+        not_found(id_customer)
 
 
-def destroy_cliente(id: str):
+def destroy_customer() -> NoReturn:
     """
      Função de exclusão de clientes
     """
-    deletado = None
-    for cliente in clientes:
-        if id_cliente == cliente.get('Id'):
-            nome = cliente.get('Nome')
-            index = clientes.index(cliente)
-            del clientes[index]
-            print(f"{nome} de id: {id_cliente} deletado com sucesso!")
-            deletado = True
+    print("Excluir um cliente")
+    id_customer = input("Digite o id do cliente que deseja excluir: ").strip()
+    excluded = None
+    for customer in customers:
+        if id_customer == customer.get('Id'):
+            name = customer.get('Name')
+            index = customers.index(customer)
+            del customers[index]
+            print(f"{name} de id: {id_customer} excluído com sucesso!")
+            excluded = True
             break
-    if not deletado:
-        print(f"Id {id_cliente} não encontrado!")
+    if not excluded:
+        not_found(id_customer)
 
 
-def contagem_regressiva(n: int):
+def contagem_regressiva(n: int) -> NoReturn:
     while n > 0:
         print(f"Saindo do sistema em {n}...")
         n -= 1
         sleep(1)
 
 
-# clientes = [{"Id": "1", "Nome": "Douglas"}]
-clientes = []
+def not_found(_id: str) -> NoReturn:
+    print(f'ID: {_id} não encontrado!')
 
-print("###########    Sistema de Cadastro de Clientes    ###########")
-while True:
+
+def menu_opcoes() -> int:
     print("1 - Cadastrar")
     print("2 - Listar")
     print("3 - Atualizar")
     print("4 - Deletar")
     print("0 - Sair")
-    menu = input("O que você deseja fazer ? ").strip()
+    return int(input("O que você deseja fazer ? ").strip())
 
-    match menu:
-        case "1":
-            nome = input("Nome do cliente: ").strip()
-            incluir_cliente(nome)
-        case "2":
-            if len(clientes) == 0:
-                print("Nenhum cliente cadastrado!")
-            [print(cliente) for cliente in clientes]
-        case "3":
-            print("Atualizar dados de um cliente")
-            id_cliente = input("Digite o id do cliente que deseja alterar: ").strip()
-            alterar_cliente(nome)
-        case "4": # do original eu troquei o pop pelo del setei a flag de deletado quando excluiu e tirei o if de
-                  # dentro do for.
-            print("Deletar um cliente")
-            id_cliente = input("Digite o id do cliente que deseja deletar: ").strip()
-            destroy_cliente(id_cliente)
-        case "0":
-            contagem_regressiva(3)
-            break
+
+def is_running() -> bool:
+    menu = menu_opcoes()
+
+    if menu == 1:
+        create_customer()
+        return True
+    elif menu == 2:
+        validate_customers()
+        list_customers()
+        return True
+    elif menu == 3:
+        update_customer()
+        return True
+    elif menu == 4:
+        destroy_customer()
+        return True
+    elif menu == 0:
+        contagem_regressiva(3)
+        return False
+    else:
+        print('Opção inválida!')
+        return True
+
+
+print("###########    Sistema de Cadastro de Clientes    ###########")
+while True:
+    if not is_running():
+        break
