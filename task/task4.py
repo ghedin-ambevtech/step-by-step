c609231c6a39f7a021defa9665388a30c44fe8d1
Alison J. Ghedin <alison.ghedin@ambevtech.com.br>
2022-05-24 23:14:59 -0300

    adicionar classe junto com as funcoes

--- task/task4.py
+++ task/task4.py
@@ -1,81 +1,92 @@
 from time import sleep
 from uuid import uuid4
+from datetime import date
 from typing import NoReturn
-
-from models.customer_model import Customer
 
 customers = []
 
 
-def validate_customers() -> NoReturn:
-    if len(customers) == 0:
-        print("Nenhum cliente cadastrado!")
+class Customer:
+    def __init__(self, id: str, name: str, age: int, address: str):
+        self.id = id
+        self.name = name
+        self.age = age
+        self.address = address
 
+    @classmethod
+    def create_customer_from_birth(cls, _id, name, year, address):
+        return cls(_id, name, date.today().year - year, address)
 
-def list_customers() -> NoReturn:
-    list_of_customers = [customer.id for customer in customers]
-    print(f"Lista de clientes: {list_of_customers}")
+    @staticmethod
+    def validate_customers() -> NoReturn:
+        if len(customers) == 0:
+            print("Nenhum cliente cadastrado!")
 
+    @staticmethod
+    def list_customers() -> NoReturn:
+        list_of_customers = [customer.id for customer in customers]
+        print(f"Lista de clientes: {list_of_customers}")
 
-def create_customer() -> NoReturn:
-    """
-     Função de cadastramento de clientes
-    """
-    id_customer = str(uuid4())[0:5]
-    name = input("Nome do cliente: ").strip().title()
-    age = int(input("Digite o ano de nascimento do cliente: "))
-    address = input("Digite o endereço do cliente: ")
-    customer = Customer.create_customer_from_birth(id_customer, name, age, address)
-    customers.append(customer)
-    print(f"Cliente {customer.name} cadastrado com sucesso!")
+    @staticmethod
+    def create_customer() -> NoReturn:
+        """
+         Função de cadastramento de clientes
+        """
+        id_customer = str(uuid4())[0:5]
+        name = input("Nome do cliente: ").strip().title()
+        age = int(input("Digite o ano de nascimento do cliente: "))
+        address = input("Digite o endereço do cliente: ")
+        customer = Customer.create_customer_from_birth(id_customer, name, age, address)
+        customers.append(customer)
+        print(f"Cliente {customer.name} cadastrado com sucesso!")
 
+    @staticmethod
+    def update_customer() -> NoReturn:
+        """
+         Função de alteração cadastral de clientes
+        """
+        print("Atualizar dados de um cliente")
+        id_customer = input("Digite o id do cliente que deseja alterar: ").strip()
+        updated = None
+        for customer in customers:
+            if id_customer == customer.id:
+                name = input(f"Você quer alterar o nome de {customer.name} para qual nome ? ").strip()
+                customer.name = name.title()
+                print(f"Cliente {customer.id} alterado com sucesso!")
+                updated = True
+                break
+        if not updated:
+            Customer.not_found(id_customer)
 
-def update_customer() -> NoReturn:
-    """
-     Função de alteração cadastral de clientes
-    """
-    print("Atualizar dados de um cliente")
-    id_customer = input("Digite o id do cliente que deseja alterar: ").strip()
-    updated = None
-    for customer in customers:
-        if id_customer == customer.id:
-            name = input(f"Você quer alterar o nome de {customer.name} para qual nome ? ").strip()
-            customer.name = name.title()
-            print(f"Cliente {customer.id} alterado com sucesso!")
-            updated = True
-            break
-    if not updated:
-        not_found(id_customer)
+    @staticmethod
+    def destroy_customer() -> NoReturn:
+        """
+         Função de exclusão de clientes
+        """
+        print("Excluir um cliente")
+        id_customer = input("Digite o id do cliente que deseja excluir: ").strip()
+        excluded = None
+        for customer in customers:
+            if id_customer == customer.id:
+                name = customer.name
+                index = customers.index(customer)
+                del customers[index]
+                print(f"{name} de id: {customer.id} excluído com sucesso!")
+                excluded = True
+                break
+        if not excluded:
+            Customer.not_found(id_customer)
 
+    @staticmethod
+    def contagem_regressiva(n: int) -> NoReturn:
+        while n > 0:
+            print(f"Saindo do sistema em {n}...")
+            n -= 1
+            sleep(1)
 
-def destroy_customer() -> NoReturn:
-    """
-     Função de exclusão de clientes
-    """
-    print("Excluir um cliente")
-    id_customer = input("Digite o id do cliente que deseja excluir: ").strip()
-    excluded = None
-    for customer in customers:
-        if id_customer == customer.id:
-            name = customer.name
-            index = customers.index(customer)
-            del customers[index]
-            print(f"{name} de id: {customer.id} excluído com sucesso!")
-            excluded = True
-            break
-    if not excluded:
-        not_found(id_customer)
-
-
-def contagem_regressiva(n: int) -> NoReturn:
-    while n > 0:
-        print(f"Saindo do sistema em {n}...")
-        n -= 1
-        sleep(1)
-
-
-def not_found(_id: str) -> NoReturn:
-    print(f'ID: {_id} não encontrado!')
+    @staticmethod
+    def not_found(_id: str) -> NoReturn:
+        print(f'ID: {_id} não encontrado!')
 
 
 def menu_opcoes() -> int:
@@ -91,20 +102,20 @@ def is_running() -> bool:
     menu = menu_opcoes()
 
     if menu == 1:
-        create_customer()
+        Customer.create_customer()
         return True
     elif menu == 2:
-        validate_customers()
-        list_customers()
+        Customer.validate_customers()
+        Customer.list_customers()
         return True
     elif menu == 3:
-        update_customer()
+        Customer.update_customer()
         return True
     elif menu == 4:
-        destroy_customer()
+        Customer.destroy_customer()
         return True
     elif menu == 0:
-        contagem_regressiva(3)
+        Customer.contagem_regressiva(3)
         return False
     else:
         print('Opção inválida!')
